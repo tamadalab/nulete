8d05367eb0142f6a78281b330f16456efc28fef8
ShokiTokunaga <g1644844@cc.kyoto-su.ac.jp>
2021-07-04 14:53:30 +0900

    initial commit

---
+++ cmd/init.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+  "os"
+  "path/filepath"
+  _ "embed"
+
+  "github.com/spf13/cobra"
+)
+
+var initCmd = &cobra.Command{
+  Use: "init",
+  Short: "make directories and generate build.gradle",
+  Run: func(cmd *cobra.Command, args []string) {
+    initCmdRun()
+  },
+}
+
+//go:embed assets/build.gradle
+var buildGradleData []byte
+
+func initCmdRun() {
+  cobra.CheckErr(os.MkdirAll(filepath.Join("src", "main", "java"), 0777))
+  cobra.CheckErr(os.MkdirAll(filepath.Join("src", "test", "java"), 0777))
+  cobra.CheckErr(os.WriteFile("build.gradle", buildGradleData, 0644))
+}
+
+func init() {
+  rootCmd.AddCommand(initCmd)
+}
